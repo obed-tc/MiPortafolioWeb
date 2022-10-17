26bdb9d07fe1e0174037c60bc90908595732cba3
obed-tc <67854304+obed-tc@users.noreply.github.com>
2022-10-17 14:20:45 +0000

    Deploying to gh-pages from @ obed-tc/MiPortafolioWeb@bfdb696753ae45d51a9e721a3266c47c71b4edc3 🚀

--- manualidadesrosita/scripts/manualidadesrosita.js
+++ manualidadesrosita/scripts/manualidadesrosita.js
@@ -1,6 +1,12 @@
-const navToggle=document.querySelector(".toggle")
-const navmenu=document.querySelector(".nav-menu")
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-navToggle.addEventListener("click",()=>{
-    navmenu.classList.toggle("navmenu_visible")
-})
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
